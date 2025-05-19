c63a4e1665391706cc2470f0b1c7065e5ab4f6e3
elleryrain <3ll3ry.r41n@gmail.com>
2025-05-19 09:28:05 +0300

    feature: switch yarn to 4.8.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import "./App.css";
 import { MainPage } from "./pages/MainPage/MainPage.tsx";
 // import { ProductsPage } from "./pages/ProductsPage/ProductsPage.tsx";
 
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 };
